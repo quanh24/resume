60ea3bf58190caab671189ff01d4bf783a18e6ea
quanh24 <43923728+quanh24@users.noreply.github.com>
2020-11-09 22:37:17 +0700

    Update scripts.js
    slide

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,18 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+var index = 0;
+showSlides();
+
+function showSlides(){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0;i<slides.length;i++){
+        slides[i].style.display = "none";
+    }
+    index++;
+    if(index > slides.length) {index = 1}
+    slides[index-1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
